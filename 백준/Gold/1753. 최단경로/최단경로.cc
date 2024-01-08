fef04092b3514c948548c4fe916a0db7aa17ae80
jun-ha <97020820+jun-ha@users.noreply.github.com>
2024-01-08 16:26:04 +0900

    [Gold IV] Title: 최단경로, Time: 116 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -15,21 +15,21 @@ using ll = long long;
 int V, E;
 #define INF 999999999
 
-void Dijkstra(vector<priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>>& path_table, vector<bool>& visited, vector<int>& shortest_table, int start);
+void Dijkstra(vector<priority_queue<pair<int, int>>>& path_table, vector<bool>& visited, vector<int>& shortest_table, int start);
 
 int main(void) {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int start;
 	cin >> V >> E;
 	cin >> start;
-	vector<priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>>path_table(V+1);
+	vector<priority_queue<pair<int, int>>>path_table(V+1);
 	vector<bool>visited(V + 1, false);
 	vector<int>shortest_table(V + 1, INF);
 	shortest_table[start] = 0;
 	for (int i = 0; i < E; i++) {
 		int from, to, weight;
 		cin >> from >> to >> weight;
-		path_table[from].push(make_pair(weight, to));
+		path_table[from].push(make_pair(-weight, to));
 		if (from == start && weight < shortest_table[to]) shortest_table[to] = weight;
 	}
 	Dijkstra(path_table, visited, shortest_table, start);
@@ -42,24 +42,24 @@ int main(void) {
 	
 	return 0;
 }
-void Dijkstra(vector<priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>>& path_table, vector<bool>&visited, vector<int>&shortest_table, int start) {
+void Dijkstra(vector<priority_queue<pair<int, int>>>& path_table, vector<bool>& visited, vector<int>& shortest_table, int start) {
 	visited[start] = true;
 	int visitCount = 0;
 	while (visitCount < V && !path_table[start].empty()) {
 		int nextIDX = path_table[start].top().second;
-		int nextIDXLen = path_table[start].top().first;
+		int nextIDXLen = -path_table[start].top().first;
 		path_table[start].pop();
 		if (visited[nextIDX] == true) continue;
 		visited[nextIDX] = true;
 		visitCount++;
 		while (!path_table[nextIDX].empty()) {
 			int next2IDX = path_table[nextIDX].top().second;
-			int next2IDXLen = path_table[nextIDX].top().first;
+			int next2IDXLen = -path_table[nextIDX].top().first;
 			path_table[nextIDX].pop();
 			if (visited[next2IDX] == true) continue;
 			if (shortest_table[next2IDX] > nextIDXLen + next2IDXLen) {
 				shortest_table[next2IDX] = nextIDXLen + next2IDXLen;
-				path_table[start].push(make_pair(nextIDXLen + next2IDXLen, next2IDX));
+				path_table[start].push(make_pair(-(nextIDXLen + next2IDXLen), next2IDX));
 			}
 		}
 	}
