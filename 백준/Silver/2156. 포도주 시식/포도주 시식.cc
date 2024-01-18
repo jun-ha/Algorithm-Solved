7439589267d6657e62537acf74ced8e907ae8349
jun-ha <97020820+jun-ha@users.noreply.github.com>
2024-01-18 15:45:58 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string>
-#include <cmath>
-#include <stack>
-#include <queue>
-#include <tuple>
-#include <map>
-#include <set>
+
 #define ENDL '\n'
 
 using namespace std;
@@ -22,12 +16,13 @@ int main(void) {
 		cin >> wines[i];
 	}
 	vector<int>accumulatedSum(3, 0);
-	accumulatedSum[0] = wines[0]; //3번째 시점부터 진행. acc[0]은 직전에 와인을 안마신상태, 
+	//3번째 와인부터 진행.
+	accumulatedSum[0] = wines[0]; //acc[0]은 직전에 와인을 안마신상태, 
 	accumulatedSum[1] = wines[1]; //acc[1]은 직전에 와인을 마신 상태
 	accumulatedSum[2] = wines[0] + wines[1]; //acc[2]는 직전 연속 두번 마신 상태
 	for (int i = 2; i < n; i++) {
 		int tmpNotDrink = max({ accumulatedSum[0], accumulatedSum[1], accumulatedSum[2] }); //와인을 이번에 안마신다면, 직전 모든 경우의 수 가능. max값 선정.
-		int tmpDrinkOnce = accumulatedSum[0] + wines[i]; //와인을 이번에 마신다면, 직전 두 번 연속은 제외하고 max 선정
+		int tmpDrinkOnce = accumulatedSum[0] + wines[i];
 		int tmpDrinkTwice = accumulatedSum[1] + wines[i];
 		accumulatedSum[0] = tmpNotDrink;
 		accumulatedSum[1] = tmpDrinkOnce;
