e97c88d715964563bc490129ac616a5169353291
jun-ha <97020820+jun-ha@users.noreply.github.com>
2024-02-08 03:07:10 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <map>
+#include <set>
+#define ENDL '\n'
+
+using namespace std;
+using ll = long long;
+
+int main(void) {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N, S;
+	cin >> N >> S;
+	vector<int>nums(N);
+	ll sum = 0;
+	for (int i = 0; i < N; i++) {
+		cin >> nums[i];
+	}
+	int front = 0, back = 0;
+	int minCount = 999999999;
+	while (back != N) {
+		sum += nums[back];
+		if (sum >= S) {
+			while (sum >= S) sum -= nums[front++];
+			front--;
+			sum+=nums[front];
+			if (minCount > back-front+1) minCount = back-front+1;
+		}
+		back++;
+	}
+	if (minCount == 999999999) cout << 0;
+	else cout << minCount;
+
+	
+	
+	return 0;
+}
