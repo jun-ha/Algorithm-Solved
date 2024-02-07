867d175b9476faaae3d79c3f39d8ebbb35bd44b4
jun-ha <97020820+jun-ha@users.noreply.github.com>
2024-02-08 04:33:42 +0900

    [Gold III] Title: 소수의 연속합, Time: 1368 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <map>
+#include <set>
+#define ENDL '\n'
+
+using namespace std;
+using ll = long long;
+
+bool isPrimeNumber(int num);
+
+int main(void) {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N; cin >> N;
+	vector<int>primeNumbers;
+	primeNumbers.push_back(2);
+	for (int num = 3; num <= 4000000; num+=2) {
+		if (isPrimeNumber(num)) {
+			primeNumbers.push_back(num);
+		}
+	}
+	int sum = 0, count = 0, front = 0, back = 0;
+	while (back != primeNumbers.size()) {
+		if (primeNumbers[back] > N) break;
+		sum += primeNumbers[back];
+		if (sum == N) count++;
+		else if (sum > N) {
+			while (sum > N && front <= back) {
+				sum -= primeNumbers[front++];
+			}
+			if (sum == N) count++;
+		}
+		back++;
+	}
+	cout << count;
+	return 0;
+}
+bool isPrimeNumber(int num) {
+	for (int i = 2; i * i <= num; i++) {
+		if (num % i == 0) return false;
+	}
+	return true;
+}
