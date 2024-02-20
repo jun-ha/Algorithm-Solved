7d318aab8b438e6eefc45f62d8af43e80ccfa962
jun-ha <97020820+jun-ha@users.noreply.github.com>
2024-02-20 15:42:30 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
@@ -1,34 +1,18 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int n, int s) {
-    vector<int> answer;
-    if(s < n) {
-        answer.push_back(-1);
-        return answer;
-    }
+    if(s < n) return {-1};
     int benchmark = s/n;
-    int sum = 0;
-    for(int i = 0; i < n; i++){
-        answer.push_back(benchmark);
-        sum += benchmark;
+    vector<int>answer(n, benchmark);
+    int left = s - benchmark*n;
+    int idx = n-1;
+    for(int i = 0; i < left; i++){
+        answer[idx]++;
+        idx--;
+        if(idx == -1) idx = n-1;
     }
-    int left = s - sum;
-    if(left > 0){
-        int idx = 0;
-        for(int i = 0; i < left; i++){
-            answer[idx]++;
-            idx++;
-            if(idx >= n) idx = 0;
-        }
-    }
-    sort(answer.begin(), answer.end());
-    
-    
-    
-    
     return answer;
 }
