fb89edf27228d8c9c86bfdc4a222985bed37cbac
jun-ha <97020820+jun-ha@users.noreply.github.com>
2024-02-22 20:24:09 +0900

    [level 2] Title: 소수 찾기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,47 +1,27 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
 bool isPrime(int k){
-    for(int i = 2; i*i <= k; i++){
-        if(k%i == 0) return false;
-    }
-    return true;
-}
-
-bool isPossibleToMake(int prime, vector<int>nums){
-    while(prime){
-        int tmpNum = prime%10;
-        if(nums[tmpNum] == 0) return false;
-        nums[tmpNum]--;
-        prime/=10;
+    if(k == 0 || k == 1) return false;
+    for(int i = 2; i*i<=k; i++){
+        if(k%i==0) return false;
     }
     return true;
 }
 
 int solution(string numbers) {
-    int answer = 0;
-    int maxNum;
-    vector<int>nums(10, 0);
-    for(int i = 0; i < numbers.length(); i++){
-        nums[numbers[i]-'0']++;
-    }
-    string maxNumStr = "";
-    for(int i = 9; i >= 0; i--){
-        for(int j = 0; j < nums[i]; j++){
-            maxNumStr += to_string(i);   
+    int size = numbers.size();
+    set<int>primes;
+    sort(numbers.begin(), numbers.end());
+    do{
+        for(int i = 1; i <= size; i++){
+            string sub = numbers.substr(0, i);
+            if(isPrime(stoi(sub))) primes.insert(stoi(sub));
         }
-    }
-    maxNum = stoi(maxNumStr);
-    vector<int>primes;
-    for(int i = 2; i <= maxNum; i++){
-        if(isPrime(i)) primes.push_back(i);
-    }
-    for(auto prime : primes){
-        if(isPossibleToMake(prime, nums)) answer++;
-    }
-    
-    return answer;
+    } while(next_permutation(numbers.begin(), numbers.end()));
+    return primes.size();
 }
