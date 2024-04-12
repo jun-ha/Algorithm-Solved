d8239d5b600da0d9e4b95fc63486364a14a2ed49
Jun ha Chang <97020820+jun-ha@users.noreply.github.com>
2024-04-12 20:18:43 +0900

    [level 0] Title: 세 개의 구분자, Time: 81.16 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,24 +1,11 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String myStr) {
-        ArrayList<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        
-        for(char c : myStr.toCharArray()){
-            if(c == 'a' || c == 'b' || c == 'c'){
-                if(sb.length() != 0) {
-                    list.add(sb.toString());
-                    sb = new StringBuilder();
-                }
-                continue;
-            }
-            sb.append(c);
-        }
-        
-        if(sb.length() != 0) {
-            list.add(sb.toString());
-        }
-        
-        return list.isEmpty() ? new String[] {"EMPTY"} : list.toArray(new String[list.size()]);
+        String[] answer = Arrays
+            .stream(myStr.split("[abc]+"))
+            .filter(s -> !s.isEmpty())
+            .toArray(String[]::new);
+        return answer.length == 0 ? new String[]{"EMPTY"} : answer;
     }
 }
