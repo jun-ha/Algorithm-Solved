5ba0216c015a2e1085a8a01f291aa31830b792db
Jun ha Chang <97020820+jun-ha@users.noreply.github.com>
2024-05-23 17:28:56 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 192 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define ENDL '\n'
+
+using namespace std;
+using ll = long long;
+
+int main(void) {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N; cin >> N;
+	vector<int>nums(N, 0);
+	for (int i = 0; i < N; i++) cin >> nums[i];
+
+	vector<int>LIS;
+	LIS.push_back(nums[0]);
+	for (int i = 0; i < N; i++) {
+		if (LIS.back() < nums[i]) {
+			LIS.push_back(nums[i]);
+			continue;
+		}			
+
+		int lessIdx = lower_bound(LIS.begin(), LIS.end(), nums[i]) - LIS.begin();
+		LIS[lessIdx] = nums[i];
+	}
+	cout << LIS.size();
+	return 0;
+}
