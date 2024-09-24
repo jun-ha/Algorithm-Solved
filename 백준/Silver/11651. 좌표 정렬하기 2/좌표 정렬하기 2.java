3516daaf1c26055ef96753d1a69d07900145d030
Junha <yy4124@gmail.com>
2024-09-24 23:18:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 644 ms, Memory: 52412 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static StringBuilder sb = new StringBuilder();
+
+    private static class Coordinate {
+        int x;
+        int y;
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Coordinate[] coordinates = new Coordinate[N];
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            coordinates[i] =  new Coordinate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        Arrays.sort(coordinates, (c1, c2) -> c1.y < c2.y ? -1 : (c1.y == c2.y ? (c1.x < c2.x) ? -1 : 1 : 1));
+
+        Arrays.stream(coordinates).forEach(c -> sb.append(c.x).append(" ").append(c.y).append('\n'));
+
+        System.out.println(sb);
+    }
+
+
+}
