94aea974cedec8daa8d3aad90f846b9091fd148e
Junha <yy4124@gmail.com>
2024-10-01 16:32:36 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 4.68 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -3,6 +3,9 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] num_list) {
-        return Math.max(IntStream.iterate(0, n -> n < num_list.length, n -> n + 2).map(n -> num_list[n]).sum(), IntStream.iterate(1, n -> n < num_list.length, n -> n + 2).map(n -> num_list[n]).sum());
+        int evenSum = IntStream.iterate(0, n -> n < num_list.length, n -> n + 2).map(n -> num_list[n]).sum();
+        int oddSum = IntStream.iterate(1, n -> n < num_list.length, n -> n + 2).map(n -> num_list[n]).sum();
+        
+        return Integer.max(evenSum, oddSum);
     }
 }
