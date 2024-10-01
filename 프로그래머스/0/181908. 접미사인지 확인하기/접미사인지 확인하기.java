9f1370a42d9f44e9a1207217828c56f304f42c30
Junha <yy4124@gmail.com>
2024-10-01 15:07:43 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.09 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int idx = my_string.lastIndexOf(is_suffix);
-        return idx < 0 ? 0 : idx + is_suffix.length() == my_string.length() ? 1 : 0;
+        return my_string.endsWith(is_suffix)? 1 : 0;
     }
 }
